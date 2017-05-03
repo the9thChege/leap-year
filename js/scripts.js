17b4212d200d7c34b1c5d8a462dd480b9401a57d
Chege <chegekformal@gmail.com>
2017-05-03 15:42:44 +0300

    Finalised the first spec

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Business Logic
+var leapYear = function(year) {
+  return false;
+};
+
+//UI Logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
